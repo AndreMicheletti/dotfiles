10993f2e4544a6558e84e7c4cbd0586fca897426
AndreMicheletti <andreluizmtmicheletti@gmail.com>
2026-04-30 15:56:23 -0300

    mods

--- install.sh
+++ install.sh
@@ -22,9 +22,9 @@ cp ./Taskfile.yml /workspaces/
 # NODE
 # install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm install --lts
 nvm use --lts
